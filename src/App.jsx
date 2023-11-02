e44b8478be095fbd2c40b45406515278d79bbf9a
Luis Alonzo <doclaag@gmail.com>
2023-11-02 12:17:18 -0600

    modified App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
-
+import { useState, useEffect } from 'react';
+import { Form } from './components/Form';
+import { Header } from './components/Header';
+import { PatientsList } from './components/PatientsList';
 
 const App = () => {
+
+  const [patients, setPatients] = useState(JSON.parse(localStorage.getItem('patients')) ?? []);
+  const [patient, setPatient] = useState({});
+
+
+  useEffect(() => {
+    localStorage.setItem('patients', JSON.stringify(patients));
+  }, [patients]);
+
+
+  const deletePatient = (id) => {
+    const newPatients = patients.filter(patient => patient.id !== id);
+    setPatients(newPatients);
+  };
+
   return (
-    <>
-      
-    </>
+    <div className='container mx-auto'>
+      <Header />
+      <div className='mt-12 md:flex'>
+        <Form
+          patients={patients}
+          setPatients={setPatients}
+          patient={patient}
+          setPatient={setPatient}
+        />
+        <PatientsList
+          patients={patients}
+          setPatient={setPatient}
+          deletePatient={deletePatient}
+        />
+      </div>
+    </div>
   );
 };
 
